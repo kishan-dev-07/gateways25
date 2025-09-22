469cca9003624c5985021ebad00748e430a92999
Kishan <kishanpradhan226@gmail.com>
2025-09-22 11:17:08 +0530

    link changes

--- src/app/e-certificate/page.js
+++ src/app/e-certificate/page.js
@@ -4,7 +4,7 @@ const ECertificatePage = () => {
   return (
     <div className="w-full h-screen overflow-hidden">
       <iframe
-        src="http://projects.abhinavjain.site/gateways/"
+        src="https://projects.abhinavjain.site/gateways/"
         className="w-full h-full border-0"
         title="E-Certificate Generator"
         allowFullScreen
