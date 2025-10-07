bbb2483d1e380c11f3730f55e0ebbeedeab98c88
Kishan <kishanpradhan226@gmail.com>
2025-10-07 11:04:49 +0530

    add certificate page for alumni talk and participation

---
+++ src/app/certificates/alumni-talk-series/page.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ECertificatePage = () => {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <iframe
+        src="https://projects.abhinavjain.site/gateways/alumni-talk-series/"
+        className="w-full h-full border-0"
+        title="E-Certificate Generator"
+        allowFullScreen
+      />
+    </div>
+  )
+}
+
+export default ECertificatePage
